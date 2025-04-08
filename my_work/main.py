599501a858a2453df40b388dadf6e8cef00fe824
Shafeen C <126206716+Shaf0@users.noreply.github.com>
2025-04-08 05:20:42 +0000

    final commit

--- my_work/main.py
+++ my_work/main.py
@@ -6,7 +6,17 @@ login = []
 # the register
 user = input("1. Login, 2. Register, 3. Quit ")
 
-# register
+## logn and checks if the username and password is in the storage
+if user == "Login":
+    input("Username? ")
+    input("Password? ")
+
+if user in "storage.csv":
+    print("Valid Confirmation!")
+
+## checks the register if it is more than 4 
+## enter username and password
+## inputs username and password into a storage
 if user == "Register":
     username = input("Enter Username: ")
     password = input("Enter Password: ")
@@ -15,19 +25,15 @@ with open("storage.csv", "a") as file:
 	writer = csv.DictWriter(file, fieldnames=["username", "password"])
 	writer.writerow({"username": username, "password": password})
 
-# login
-if user == "Login":
-    input("Username? ")
-    input("Password? ")
-
-#quit
-    exit()
-    
-
+if password < 4:
+    x = password
+    while x < 4:
+        print("Must be a 4+ letter password")
 
 #incorrect password
 
 #incorrect username 
 
-# logout
+# quit
 
+# logout
